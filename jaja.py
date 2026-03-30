86429969c444e02f4c339969e87cb6d9931779ef
julianopdf <julianogaitaman@gmail.com>
2026-03-30 07:01:11 -0300

    bora caquedo

--- jaja.py
+++ jaja.py
@@ -1,4 +1,10 @@
 def jaja():
-    print('jajajjaa')
+    name = input(" write shit: ")
+    if (name != "shit"):
+        print('jajajjaa')
+    else:
+        # This print statement must be indented!
+        print('vsf')
+
 while(1):   
     jaja()
